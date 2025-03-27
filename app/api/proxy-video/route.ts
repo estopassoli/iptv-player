843f5e1f968c8036ac7ad12af37a1f84a6826283
estopassoli <itsdenahi@gmail.com>
2025-03-27 20:20:09 -0300

    fix: add fix to proxy video

--- app/api/proxy-video/route.ts
+++ app/api/proxy-video/route.ts
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
     const rangeHeader = request.headers.get("range")
 
     // Configurar headers para a requisição
-    const headers = {
+    const headers: Record<string, string> = {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     }
